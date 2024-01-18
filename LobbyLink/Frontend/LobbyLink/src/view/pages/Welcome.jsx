421e4e90e8ec55d618ed2bc8b2b6a3892e792902
Tommy Goodman <tommysdevelopment@gmail.com>
2024-01-18 20:10:15 +1300

    Welcome page designed and coded. -added assets On branch pages Your branch is up to date with 'origin/pages'.
    Changes to be committed:
    	modified:   LobbyLink/Frontend/LobbyLink/index.html
    	new file:   LobbyLink/Frontend/LobbyLink/public/logo-dark-375.png
    	new file:   LobbyLink/Frontend/LobbyLink/public/logo-icon.ico
    	modified:   LobbyLink/Frontend/LobbyLink/src/App.css
    	modified:   LobbyLink/Frontend/LobbyLink/src/App.jsx
    	new file:   LobbyLink/Frontend/LobbyLink/src/assets/logo-dark-500.png
    	new file:   LobbyLink/Frontend/LobbyLink/src/assets/logo-dark.svg
    	deleted:    LobbyLink/Frontend/LobbyLink/src/assets/react.svg
    	new file:   LobbyLink/Frontend/LobbyLink/src/css/Welcome.css
    	modified:   LobbyLink/Frontend/LobbyLink/src/index.css
    	modified:   LobbyLink/Frontend/LobbyLink/src/main.jsx
    	modified:   LobbyLink/Frontend/LobbyLink/src/view/pages/Welcome.jsx

--- LobbyLink/Frontend/LobbyLink/src/view/pages/Welcome.jsx
+++ LobbyLink/Frontend/LobbyLink/src/view/pages/Welcome.jsx
@@ -1,8 +1,50 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Stack from "react-bootstrap/Stack";
+import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
+import "../../css/Welcome.css";
+
 const Welcome = (params) => {
     return (
-        <div>
-            <h1>Welcome</h1>
-        </div>
+        <Container
+            fluid
+            className="welcome-container">
+            <Row>
+                <Col
+                    md="6"
+                    className="welcome-left d-flex flex-column justify-content-center min-vh-100">
+                    <Image
+                        src="/logo-dark-375.png"
+                        roundedCircle
+                        className="welcome-logo align-self-center"
+                    />
+                    <h2 className="welcome-logo-subtext-1">Welcome to</h2>
+                    <h1 className="welcome-logo-subtext-2">LobbyLink</h1>
+                </Col>
+                <Col
+                    md="6"
+                    className="welcome-right d-flex flex-column justify-content-center min-vh-100">
+                    <p className="please-text">
+                        Please sign in & out
+                        <br />
+                        Upon arrival or departure
+                    </p>
+
+                    <Button
+                        size="lg"
+                        className="btn-sign-in">
+                        SIGN IN
+                    </Button>
+                    <Button
+                        size="lg"
+                        className="btn-sign-out">
+                        SIGN OUT
+                    </Button>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
