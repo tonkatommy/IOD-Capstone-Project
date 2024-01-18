f4b287909f162a4c6aa5174a7425ddbae304d9fa
Tommy Goodman <tommysdevelopment@gmail.com>
2024-01-18 20:27:00 +1300

    Created new files for SignIn page and SigIn css On branch sign-in-screen Your branch is up to date with 'origin/sign-in-screen'.
    Changes to be committed:
    	new file:   LobbyLink/Frontend/LobbyLink/src/css/SignIn.css
    	new file:   LobbyLink/Frontend/LobbyLink/src/view/pages/SignIn.jsx

---
+++ LobbyLink/Frontend/LobbyLink/src/view/pages/SignIn.jsx
@@ -0,0 +1,11 @@
+import "../../css/SignIn.css";
+
+const SignInPage = (params) => {
+    return (
+        <div>
+            <h1>Sign In Page</h1>
+        </div>
+    );
+};
+
+export default SignInPage;
