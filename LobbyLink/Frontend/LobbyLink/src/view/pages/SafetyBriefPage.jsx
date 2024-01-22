ef0592f59695d170a805559c98129f87ab37f0b5
Tommy Goodman <tommysdevelopment@gmail.com>
2024-01-22 17:00:00 +1300

    Removed react-pdf and installed pdfjs-dist
    On branch safety-brief-page
    Your branch is up to date with 'origin/safety-brief-page'.
    
    Changes to be committed:
    	new file:   Assets/Health & Safety Brief.pdf
    	modified:   LobbyLink/Frontend/LobbyLink/package-lock.json
    	modified:   LobbyLink/Frontend/LobbyLink/package.json
    	new file:   LobbyLink/Frontend/LobbyLink/public/Health & Safety Brief.pdf
    	new file:   LobbyLink/Frontend/LobbyLink/src/assets/Health & Safety Brief.pdf
    	modified:   LobbyLink/Frontend/LobbyLink/src/view/pages/SafetyBriefPage.jsx

--- LobbyLink/Frontend/LobbyLink/src/view/pages/SafetyBriefPage.jsx
+++ LobbyLink/Frontend/LobbyLink/src/view/pages/SafetyBriefPage.jsx
@@ -1,4 +1,6 @@
 import Container from "react-bootstrap/esm/Container";
+import { Document, Page } from "react-pdf";
+import React from "react";
 import "../../css/SafetyBrief.css";
 
 const SafetyBriefPage = (params) => {
@@ -10,6 +12,9 @@ const SafetyBriefPage = (params) => {
                 fluid
                 className="safetyBriefPage-inner-container">
                 Safety Brief Content
+                <Document file="../../assets/Health & Safety Brief.pdf">
+                    <Page />
+                </Document>
             </Container>
         </Container>
     );
