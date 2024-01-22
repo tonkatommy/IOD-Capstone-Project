d0775f4a0f06268eb435cbb7f381ed1981e945e4
Tommy Goodman <tommysdevelopment@gmail.com>
2024-01-22 16:00:21 +1300

    Safet Brief Page created
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   LobbyLink/Frontend/LobbyLink/src/App.jsx
    	new file:   LobbyLink/Frontend/LobbyLink/src/css/SafetyBrief.css
    	new file:   LobbyLink/Frontend/LobbyLink/src/view/pages/SafetyBriefPage.jsx

--- LobbyLink/Frontend/LobbyLink/src/App.jsx
+++ LobbyLink/Frontend/LobbyLink/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 // import WelcomePage from "./view/pages/WelcomePage";
 import SignInPage from "./view/pages/SignInPage";
+import SafetyBriefPage from "./view/pages/SafetyBriefPage";
 
 function App() {
     const [count, setCount] = useState(0);
@@ -9,7 +10,8 @@ function App() {
     return (
         <>
             {/* <WelcomePage /> */}
-            <SignInPage />
+            {/* <SignInPage /> */}
+            <SafetyBriefPage />
         </>
     );
 }
---
+++ LobbyLink/Frontend/LobbyLink/src/view/pages/SafetyBriefPage.jsx
@@ -0,0 +1,18 @@
+import Container from "react-bootstrap/esm/Container";
+import "../../css/SafetyBrief.css";
+
+const SafetyBriefPage = (params) => {
+    return (
+        <Container
+            fluid
+            className="safetyBriefPage-container">
+            <Container
+                fluid
+                className="safetyBriefPage-inner-container">
+                Hello
+            </Container>
+        </Container>
+    );
+};
+
+export default SafetyBriefPage;
