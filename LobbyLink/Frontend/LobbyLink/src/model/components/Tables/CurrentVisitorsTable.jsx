e3a6781e1b87178c4d4ad255327ca56bd9f12250
Tommy Goodman <tommysdevelopment@gmail.com>
2024-01-28 12:13:39 +1300

    Current Visitors table populated with mock data
    On branch current-visitors-table
    Your branch is up to date with 'origin/current-visitors-table'.
    
    Changes to be committed:
    	modified:   LobbyLink/Frontend/LobbyLink/src/model/components/Tables/CurrentVisitorsTable.jsx

--- LobbyLink/Frontend/LobbyLink/src/model/components/Tables/CurrentVisitorsTable.jsx
+++ LobbyLink/Frontend/LobbyLink/src/model/components/Tables/CurrentVisitorsTable.jsx
@@ -10,29 +10,38 @@ const CurrentVisitorsTable = (params) => {
                 hover>
                 <thead>
                     <tr>
-                        <th>#</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Username</th>
+                        <th>ID#</th>
+                        <th>Name</th>
+                        <th>Origin</th>
+                        <th>Destination</th>
+                        <th>Contact No.</th>
+                        <th>Time In</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td>1</td>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
+                        <td>Y1025132</td>
+                        <td>CPL, Tommy Goodman</td>
+                        <td>Home</td>
+                        <td>Electrical Systems</td>
+                        <td>021123456</td>
+                        <td>12:06 Sun 28/1/2024</td>
                     </tr>
                     <tr>
-                        <td>2</td>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>X1023423</td>
+                        <td>SGT, Jimmy Calray</td>
+                        <td>Rocket Labs</td>
+                        <td>E3/Capabilities Bay</td>
+                        <td>027456852</td>
+                        <td>13:10 Sun 28/1/2024</td>
                     </tr>
                     <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
+                        <td>Q1057384</td>
+                        <td>Mrs, Emmy Newton</td>
+                        <td>ANZ</td>
+                        <td>AVSQN HQ</td>
+                        <td>021555444</td>
+                        <td>14:27 Sun 28/1/2024</td>
                     </tr>
                 </tbody>
             </Table>
@@ -41,3 +50,5 @@ const CurrentVisitorsTable = (params) => {
 };
 
 export default CurrentVisitorsTable;
+
+//  "SRVC NO", "RANK", "FIRST NAME", "LAST NAME", "WORK PLACE", "CONTACT NO", "TIME IN")
