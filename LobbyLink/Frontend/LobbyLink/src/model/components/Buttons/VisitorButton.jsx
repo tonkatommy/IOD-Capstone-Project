3fdc64cd53f17ca2e381c53848100ae19e27c664
Tommy Goodman <tommysdevelopment@gmail.com>
2024-01-29 09:38:38 +1300

    visitor-button changes

--- LobbyLink/Frontend/LobbyLink/src/model/components/Buttons/VisitorButton.jsx
+++ LobbyLink/Frontend/LobbyLink/src/model/components/Buttons/VisitorButton.jsx
@@ -1,5 +1,7 @@
 import Button from "react-bootstrap/esm/Button";
 import SignOutButton from "./SignOutButton";
+import Col from "react-bootstrap/esm/Col";
+import Row from "react-bootstrap/esm/Row";
 import "../../../css/components/VisitorButton.css";
 
 const VisitorButton = (params) => {
@@ -12,15 +14,27 @@ const VisitorButton = (params) => {
             class="visitor-btn-group btn-group"
             role="group"
             aria-label="Basic example">
-            <Button className="visitor-button d-flex gap-2 align-items-center justify-content-start">
-                <i class="bi bi-person-fill"></i>
-                <span className="visitor-name me-2">CPL, Tommy Goodman</span>
-                <i class="bi bi-signpost-split-fill"></i>
-                <span className="visitor-destination me-auto">
-                    Instrument Systems
-                </span>
-                <i class="bi bi-watch"></i>
-                <span className="visitor-time-in ">10:13 Sun 28th Jan</span>
+            <Button className="visitor-button">
+                <Row>
+                    <Col className="d-flex gap-2 align-items-center">
+                        <i class="bi bi-person-fill"></i>
+                        <span className="visitor-name me-2">
+                            CPL, Tommy Goodman
+                        </span>
+                    </Col>
+                    <Col className="d-flex gap-2 align-items-center">
+                        <i class="bi bi-signpost-split-fill"></i>
+                        <span className="visitor-destination me-auto">
+                            Instrument Systems
+                        </span>
+                    </Col>
+                    <Col className="d-flex gap-2 align-items-center">
+                        <i class="bi bi-watch"></i>
+                        <span className="visitor-time-in ">
+                            10:13 Sun 28th Jan
+                        </span>
+                    </Col>
+                </Row>
             </Button>
             <SignOutButton
                 className="visitor-button-sign-out flex-grow-0"
