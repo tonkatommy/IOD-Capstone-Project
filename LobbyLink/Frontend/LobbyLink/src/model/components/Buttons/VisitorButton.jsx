d60f6feef6f2e71cd99a34cbfa2a26509e024eaa
Tommy Goodman <tommysdevelopment@gmail.com>
2024-01-28 22:26:45 +1300

    Visitor Button styles finished
    On branch visitor-button
    Your branch is up to date with 'origin/visitor-button'.
    
    Changes to be committed:
    	modified:   LobbyLink/Frontend/LobbyLink/index.html
    	modified:   LobbyLink/Frontend/LobbyLink/src/css/components/VisitorButton.css
    	modified:   LobbyLink/Frontend/LobbyLink/src/model/components/Buttons/VisitorButton.jsx

--- LobbyLink/Frontend/LobbyLink/src/model/components/Buttons/VisitorButton.jsx
+++ LobbyLink/Frontend/LobbyLink/src/model/components/Buttons/VisitorButton.jsx
@@ -12,13 +12,15 @@ const VisitorButton = (params) => {
             class="visitor-btn-group btn-group"
             role="group"
             aria-label="Basic example">
-            <Button className="visitor-button d-flex gap-2 justify-content-start">
+            <Button className="visitor-button d-flex gap-2 align-items-center justify-content-start">
                 <i class="bi bi-person-fill"></i>
-                <span className="visitor-name">CPL, Tommy Goodman</span>
+                <span className="visitor-name me-2">CPL, Tommy Goodman</span>
                 <i class="bi bi-signpost-split-fill"></i>
-                <span className="visitor-destination">Instrument Systems</span>
+                <span className="visitor-destination me-auto">
+                    Instrument Systems
+                </span>
                 <i class="bi bi-watch"></i>
-                <span className="visitor-time-in">10:13 Sun 28th Jan</span>
+                <span className="visitor-time-in ">10:13 Sun 28th Jan</span>
             </Button>
             <SignOutButton
                 className="visitor-button-sign-out flex-grow-0"
