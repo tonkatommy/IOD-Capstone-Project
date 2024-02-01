b5cfb0f65aecd40e7b23170bf0e755d30b75139c
Tommy Goodman <tommysdevelopment@gmail.com>
2024-02-01 15:23:18 +1300

    Added content to database table. Added button text.
    Changes to be committed:
    	modified:   LobbyLink/Frontend/LobbyLink/src/css/pages/Database.css
    	modified:   LobbyLink/Frontend/LobbyLink/src/css/pages/Details.css
    	modified:   LobbyLink/Frontend/LobbyLink/src/model/components/Tables/DatabaseUsersTable.jsx
    	modified:   LobbyLink/Frontend/LobbyLink/src/view/pages/DatabasePage.jsx

--- LobbyLink/Frontend/LobbyLink/src/model/components/Tables/DatabaseUsersTable.jsx
+++ LobbyLink/Frontend/LobbyLink/src/model/components/Tables/DatabaseUsersTable.jsx
@@ -8,7 +8,30 @@ const DatabaseUsersTable = (params) => {
         <>
             <Container
                 fluid
-                className="db-table-container"></Container>
+                className="db-table-container p-0">
+                <table className="table rounded-3 overflow-hidden">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID #</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">First Name</th>
+                            <th scope="col">Last Name</th>
+                            <th scope="col">Origin</th>
+                            <th scope="col">Contact</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">Y1025132</th>
+                            <td>CPL</td>
+                            <td>Thomas</td>
+                            <td>Goodman</td>
+                            <td>TommyTinkersNZ</td>
+                            <td>021-456-123</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </Container>
         </>
     );
 };
--- LobbyLink/Frontend/LobbyLink/src/view/pages/DatabasePage.jsx
+++ LobbyLink/Frontend/LobbyLink/src/view/pages/DatabasePage.jsx
@@ -2,16 +2,58 @@ import RedGradientBG from "../../model/components/Background/RedGradientBG";
 import DatabaseUsersTable from "../../model/components/Tables/DatabaseUsersTable";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
+import DBButton from "../../model/components/Buttons/DBButton";
+import Container from "react-bootstrap/esm/Container";
 
 const DatabasePage = (props) => {
     return (
         <>
             <RedGradientBG />
-            <Row>
-                <Col>
-                    <DatabaseUsersTable />
-                </Col>
-            </Row>
+            <Container
+                fluid
+                className="db-container p-5">
+                <Row className="">
+                    <p className="db-title text-start fs-1 fw-bold">
+                        Database Management
+                    </p>
+                </Row>
+                <Row className="">
+                    <Col className="col-db-controls col-3 d-flex flex-column ">
+                        <p className="text-start fs-3 fw-bold">DB Controls</p>
+                        <p className="text-start mb-0 fs-6 ">
+                            Add a new User to the DB:
+                        </p>
+                        <DBButton
+                            className="btn-db btn-db-add-user mb-3"
+                            buttonText="Add User"
+                        />
+                        <p className="text-start mb-0 fs-6 ">
+                            Remove a User from the DB:
+                        </p>
+                        <DBButton
+                            className="btn-db btn-db-remove-user mb-3"
+                            buttonText="Remove User"
+                        />
+                        <p className="text-start mb-0 fs-6 ">
+                            Set user type to Employee:
+                        </p>
+                        <DBButton
+                            className="btn-db btn-db-set-employee mb-3"
+                            buttonText="Set Employee"
+                        />
+                        <p className="text-start mb-0 fs-6 ">
+                            Set User type to Visitor:
+                        </p>
+                        <DBButton
+                            className="btn-db btn-db-set-visitor mb-3"
+                            buttonText="Set Visitor"
+                        />
+                    </Col>
+                    <Col className="col-db-table">
+                        <DatabaseUsersTable />
+                    </Col>
+                </Row>
+            </Container>
         </>
     );
 };
